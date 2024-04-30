494dd797636ca99bc66e94e317e4b4d1851c4a54
akandra <akandra@mpibpc.mpg.de>
2024-04-30 16:00:29 +0200

    format of interaction section of energy file is changed from law species lst species lst values to law species lst ast species lst ast values The modules dealing with interaction energy are correspondingly modified

--- energy_mod.f90
+++ energy_mod.f90
@@ -20,7 +20,7 @@ real(dp) function energy(ref, lat, e_pars)
 
   integer :: i, i_shell, i_n
   integer :: ref_row, ref_col, ref_ast, ref_id, ref_lst
-  integer :: row, col, id
+  integer :: row, col, id, lst, ast
   real(8) :: energy_acc
 
   ref_row = lat%ads_list(ref)%row
@@ -38,20 +38,22 @@ real(dp) function energy(ref, lat, e_pars)
 
       call lat%neighbor(ref, i, row, col, i_shell)
       i_n = lat%occupations(row,col)
+      lst = lat%lst(row,col)
 
       if (i_n > 0) then
 
-        id = lat%ads_list(i_n)%id
-        if (e_pars%int_energy_skip(ref_id, ref_lst, id, lat%lst(row,col), i_shell)) cycle
+        id  = lat%ads_list(i_n)%id
+        ast = lat%ads_list(i_n)%ast
+        if (e_pars%int_energy_skip(ref_id, ref_lst, ref_ast, id, lst, ast, i_shell)) cycle
 
-        if (e_pars%int_energy_law_id(ref_id, ref_lst, id, lat%lst(row,col)) /= linear_id) then
+        if (e_pars%int_energy_law_id(ref_id, ref_lst, ref_ast, id, lst, ast) /= linear_id) then
           print'(A,i3,A)', "module energy.f90: function energy: interaction law with id = ", &
-                  e_pars%int_energy_law_id(ref_id, ref_lst, id, lat%lst(row,col)), ' is not yet implemented.'
+                  e_pars%int_energy_law_id(ref_id, ref_lst, ref_ast, id, lst, ast), ' is not yet implemented.'
           stop
 
         end if
 
-        energy_acc = energy_acc + e_pars%int_energy_pars(ref_id, ref_lst, id, lat%lst(row,col),i_shell)
+        energy_acc = energy_acc + e_pars%int_energy_pars(ref_id, ref_lst, ref_ast, id, lst, ast, i_shell)
 
       end if
 
@@ -71,7 +73,7 @@ real(dp) function total_energy(lat, e_pars)
 
   integer :: i, i_shell, i_n, ref
   integer :: ref_row, ref_col, ref_ast, ref_lst, ref_id
-  integer :: row, col, id
+  integer :: row, col, id, lst, ast
   real(8) :: energy_acc
 
   energy_acc = 0
@@ -92,20 +94,22 @@ real(dp) function total_energy(lat, e_pars)
 
         call lat%neighbor(ref, i, row, col, i_shell)
         i_n = lat%occupations(row,col)
+        lst = lat%lst(row,col)
 
         if (i_n > 0) then
 
           if (i_n < ref) cycle ! Avoid double counting
-          id = lat%ads_list(i_n)%id
-          if (e_pars%int_energy_skip(ref_id, ref_lst, id, lat%lst(row,col),i_shell)) cycle
+          id  = lat%ads_list(i_n)%id
+          ast = lat%ads_list(i_n)%ast
+          if (e_pars%int_energy_skip(ref_id, ref_lst, ref_ast, id, lst, ast,i_shell)) cycle
 
-          if (e_pars%int_energy_law_id(ref_id, ref_lst, id, lat%lst(row, col)) /= linear_id) then
+          if (e_pars%int_energy_law_id(ref_id, ref_lst, ref_ast, id, lst, ast) /= linear_id) then
             print'(A,i3,A)', "module energy.f90: function energy: interaction law with id = ", &
-                    e_pars%int_energy_law_id(ref_id, ref_lst, id, lat%lst(row,col)), ' is not yet implemented.'
+                    e_pars%int_energy_law_id(ref_id, ref_lst, ref_ast, id, lst, ast), ' is not yet implemented.'
             stop
 
           end if
-          energy_acc = energy_acc + e_pars%int_energy_pars(ref_id, ref_lst, id, lat%lst(row,col),i_shell)
+          energy_acc = energy_acc + e_pars%int_energy_pars(ref_id, ref_lst, ref_ast, id, lst, ast, i_shell)
 
         end if
 
@@ -120,43 +124,5 @@ real(dp) function total_energy(lat, e_pars)
 
 end function
 
-!
-!real(8) function total_energy(nlat, nads, nnn, occupations, site_type, &
-!                              ads_list, nn_list, ads_energy, int_energy)
-!
-!integer, intent(in) :: nlat, nads, nnn
-!integer, dimension(nlat,nlat), intent(in) :: occupations
-!integer(1), dimension(nlat,nlat), intent(in) :: site_type
-!integer, dimension(nads,2), intent(in) :: ads_list
-!integer, dimension(nnn,2), intent(in) :: nn_list
-!real(8), dimension(3), intent(in) :: ads_energy
-!real(8), dimension(3,3), intent(in) :: int_energy
-!
-!integer :: i, ic, jc, iads, jads
-!real(8) :: energy_acc
-!
-!    energy_acc = 0.0d0
-!    do inx=1,nads
-!
-!        iads = ads_list(inx,1)
-!        jads = ads_list(inx,2)
-!
-!        energy_acc = energy_acc + ads_energy(site_type(iads,jads))
-!
-!        do i=1, nnn/2 ! count interaction with neighbors once
-!
-!            ic = modulo(iads+nn_list(i,1)-1,nlat) + 1
-!            jc = modulo(jads+nn_list(i,2)-1,nlat) + 1
-!            if (occupations(ic,jc) > 0) &
-!                energy_acc = energy_acc +&
-!                        int_energy(site_type(iads,jads),site_type(ic,jc))
-!
-!        end do
-!
-!    end do
-!
-!    total_energy = energy_acc
-!
-!end function
 
 end module energy_mod
--- energy_parameters_class.f90
+++ energy_parameters_class.f90
@@ -13,13 +13,13 @@ module energy_parameters_class
 
     ! Adsorpton energies (n_species x n_site_type x n_adsorption_sites)
     real(dp), dimension(:,:,:), allocatable :: ads_energy
-    ! Interaction energy law id (n_species x n_site_type x n_species x n_site_type)
-    integer, dimension(:,:,:,:), allocatable :: int_energy_law_id
-    ! Interaction energy (n_species x n_site_type x n_species x n_site_type x n_shells )
-    real(dp), dimension(:,:,:,:,:),allocatable :: int_energy_pars
+    ! Interaction energy law id (n_species x n_site_type x n_adsorption_sites x n_species x n_site_type x n_adsorption_sites)
+    integer, dimension(:,:,:,:,:,:), allocatable :: int_energy_law_id
+    ! Interaction energy (n_species x n_site_type x n_adsorption_sites x n_species x n_site_type x n_adsorption_sites x n_shells )
+    real(dp), dimension(:,:,:,:,:,:,:),allocatable :: int_energy_pars
     ! Interaction energy mask (n_species x n_species x n_shells)
     ! .true. means to skip interaction
-    logical, dimension(:,:,:,:,:),allocatable :: int_energy_skip
+    logical, dimension(:,:,:,:,:,:,:),allocatable :: int_energy_skip
     !
     logical :: is_interaction
     ! Default value for undefined energy
@@ -41,7 +41,7 @@ contains
 
     type(control_parameters), intent(inout) :: control_pars
 
-    integer :: i, ios, nwords, line_number, i1, i1s, i2, i2s, i_law
+    integer :: i, ios, nwords, line_number, i1, i1s, i1a, i2, i2s, i2a, i_law
 
     character(len=max_string_length) :: buffer
     character(len=max_string_length) :: words(100)
@@ -61,9 +61,12 @@ contains
 
     i = control_pars%n_species
     allocate(energy_parameters_init%ads_energy(i,n_max_lat_site_types,n_max_ads_sites))
-    allocate(energy_parameters_init%int_energy_law_id(i,n_max_lat_site_types,i,n_max_lat_site_types))
-    allocate(energy_parameters_init%int_energy_pars(i,n_max_lat_site_types,i,n_max_lat_site_types,n_shells))
-    allocate(energy_parameters_init%int_energy_skip(i,n_max_lat_site_types,i,n_max_lat_site_types,n_shells))
+    allocate(energy_parameters_init%int_energy_law_id(i,n_max_lat_site_types,n_max_ads_sites,&
+                                                      i,n_max_lat_site_types,n_max_ads_sites))
+    allocate(energy_parameters_init%int_energy_pars(i,n_max_lat_site_types,n_max_ads_sites,&
+                                                    i,n_max_lat_site_types,n_max_ads_sites,n_shells))
+    allocate(energy_parameters_init%int_energy_skip(i,n_max_lat_site_types,n_max_ads_sites,&
+                                                    i,n_max_lat_site_types,n_max_ads_sites,n_shells))
 
     energy_parameters_init%ads_energy = default_dp
     energy_parameters_init%int_energy_law_id = default_int
@@ -119,9 +122,12 @@ contains
           i1 = get_index(words(1),lat_site_names)
           i2 = get_index(words(2),ads_site_names)
 
-          if ( i1==0 .or. i2==0 ) &
+          if ( i1==0 ) &
               call error_message(file_name, line_number, buffer, &
-                           "wrong species name in the adsorption section")
+                           "unknown lattice site type in the adsorption section")
+          if ( i2==0 ) &
+              call error_message(file_name, line_number, buffer, &
+                           "unknown adsorption site type in the adsorption section")
           if (energy_parameters_init%ads_energy(current_species_id,i1,i2 ) /= default_dp)&
               call error_message(file_name, line_number, buffer, "duplicated entry")
 
@@ -148,29 +154,36 @@ contains
           if (parse_state /= parse_state_interaction) &
             call error_message(file_name, line_number, buffer, &
                       "invalid interaction law statement")
-          if (nwords/=5+n_shells) call error_message(file_name, line_number, buffer, &
-                            "interaction law key must have (5 + n_shells) parameters")
+          if (nwords/=7+n_shells) call error_message(file_name, line_number, buffer, &
+                            "interaction law key must have (7 + n_shells) parameters")
 
           i_law  = get_index(words(1),int_law_names)
           i1  = get_index(words(2),control_pars%ads_names)
           i1s = get_index(words(3),lat_site_names)
-          i2  = get_index(words(4),control_pars%ads_names)
-          i2s = get_index(words(5),lat_site_names)
+          i1a = get_index(words(4),ads_site_names)
+          i2  = get_index(words(5),control_pars%ads_names)
+          i2s = get_index(words(6),lat_site_names)
+          i2a = get_index(words(7),ads_site_names)
 
           if ( i1==0 .or. i2==0 ) call error_message(file_name, line_number, buffer, &
-                            "wrong species name in the interaction law")
-          if (energy_parameters_init%int_energy_law_id(i1,i1s,i2,i2s) /= default_int)&
-              call error_message(file_name, line_number, buffer, "duplicated entry for species")
-          energy_parameters_init%int_energy_law_id(i1,i1s,i2,i2s) = i_law
-          energy_parameters_init%int_energy_law_id(i2,i2s,i1,i1s) = i_law
+                            "unknown species name in the interaction law")
+          if ( i1s==0 .or. i2s==0 ) &
+              call error_message(file_name, line_number, buffer, &
+                           "unknown lattice site type in the interaction section")
+          if ( i1a==0 .or. i2a==0 ) &
+              call error_message(file_name, line_number, buffer, &
+                           "unknown adsorption site type in the interaction section")
+          if (energy_parameters_init%int_energy_law_id(i1,i1s,i1a,i2,i2s,i2a) /= default_int)&
+              call error_message(file_name, line_number, buffer, "duplicated entry in the interaction section")
+          energy_parameters_init%int_energy_law_id(i1,i1s,i1a,i2,i2s,i2a) = i_law
+          energy_parameters_init%int_energy_law_id(i2,i2s,i2a,i1,i1s,i1a) = i_law
           do i=1, n_shells
-            if (read_num(words(5+i),energy_parameters_init%int_energy_pars(i1,i1s,i2,i2s,i)))&
-              energy_parameters_init%int_energy_skip(i1,i1s,i2,i2s,i) = .false.
-!              read(words(3+i),*) energy_parameters_init%int_energy_pars(i1,i2,i)
-            energy_parameters_init%int_energy_pars(i2,i2s,i1,i1s,i) = &
-                                energy_parameters_init%int_energy_pars(i1,i1s,i2,i2s,i)
-            energy_parameters_init%int_energy_skip(i2,i2s,i1,i1s,i) = &
-                                energy_parameters_init%int_energy_skip(i1,i1s,i2,i2s,i)
+            if (read_num(words(7+i),energy_parameters_init%int_energy_pars(i1,i1s,i1a,i2,i2s,i2a,i)))&
+              energy_parameters_init%int_energy_skip(i1,i1s,i1a,i2,i2s,i2a,i) = .false.
+            energy_parameters_init%int_energy_pars(i2,i2s,i2a,i1,i1s,i1a,i) = &
+                                energy_parameters_init%int_energy_pars(i1,i1s,i1a,i2,i2s,i2a,i)
+            energy_parameters_init%int_energy_skip(i2,i2s,i2a,i1,i1s,i1a,i) = &
+                                energy_parameters_init%int_energy_skip(i1,i1s,i1a,i2,i2s,i2a,i)
           end do
 !            print*, 'int. law: ', energy_parameters_init%int_energy_law_id(i1,i2),&
 !                    ' for species 1:', i1,&
